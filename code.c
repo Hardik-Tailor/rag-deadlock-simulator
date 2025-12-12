8d09ce297ca39fe7b0445bc947e95229396ffc5a
Rojer Hein <164485492+HeinRojer@users.noreply.github.com>
2025-12-12 21:39:53 +0530

    Implement addProcess and addResource functions
    Added addProcess() and addResource() functions

--- code.c
+++ code.c
@@ -2,15 +2,29 @@
 #include <stdbool.h>
 
 #define MAX_NODES 50
-
-int graph[MAX_NODES][MAX_NODES];  
-int nodeType[MAX_NODES];          
-int nodeCount = 0;                
-
 #define PROCESS 1
 #define RESOURCE 2
 
+int graph[MAX_NODES][MAX_NODES];
+int nodeType[MAX_NODES];
+int nodeCount = 0;
+
+void addProcess() {
+    nodeType[nodeCount] = PROCESS;
+    printf("Process P%d created.\n", nodeCount);
+    nodeCount++;
+}
+
+void addResource() {
+    nodeType[nodeCount] = RESOURCE;
+    printf("Resource R%d created.\n", nodeCount);
+    nodeCount++;
+}
+
 int main() {
-    printf("Data structures initialized for RAG.\n");
+    
+    addProcess();
+    addResource();
     return 0;
 }
+
